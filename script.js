2193f935a79a8690b14c162985620b11656f456b
berru-g <g.leberruyer@gmail.com>
2024-06-16 02:58:57 +0200

    front

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
 
 const tracks = [
-    { title: 'Boule', file: './2024/BOULE.wav', backgroundClass: 'bg-animation-1' },
-    { title: 'Encore', file: './2024/ENCORE.wav', backgroundClass: 'bg-animation-2' },
+    { title: 'Boule', file: 'https://github.com/berru-g/gangue-astral-free-player/raw/main/2024/BOULE.wav', backgroundClass: 'bg-animation-1' },
+    { title: 'Encore', file: 'https://github.com/berru-g/gangue-astral-free-player/raw/main/2024/ENCORE.wav', backgroundClass: 'bg-animation-2' },
     { title: 'ff', file: './2024/ff.wav', backgroundClass: 'bg-animation-3' },
-    { title: 'SOFT', file: './2024/SOFT.wav', backgroundClass: 'bg-animation-3' },
+    { title: 'SOFT', file: './2024/SOFT.wav', backgroundClass: 'bg-animation-1'},
     { title: 'OSC', file: './2024/OSC.wav', background: 'url_to_background_image2.jpg' },
-    { title: 'Utopia', file: './2023/UTOPIA.wav', background: 'url_to_background_image3.jpg' },
-    { title: 'Romance', file: './2023/ROMANCE.wav', background: 'url_to_background_image3.jpg' },
-    { title: 'piste 8', file: './2024/OSC.wav', background: 'url_to_background_image2.jpg' },
-    { title: 'piste 9', file: './2023/UTOPIA.wav', background: 'url_to_background_image3.jpg' },
-    { title: 'piste 10', file: './2023/ROMANCE.wav', background: 'url_to_background_image3.jpg' }
+    { title: 'Track 6', file: './2024/', background: 'url_to_background_image3.jpg' },
+    { title: 'Track 7', file: './2024/', background: 'url_to_background_image3.jpg' },
+    { title: 'Track 8', file: './2024/', background: 'url_to_background_image3.jpg' },
+    { title: 'Track 9', file: './2024/', background: 'url_to_background_image3.jpg' }
+  
+  
+  
 ];
 
 const playlist = document.getElementById('playlist');
@@ -79,8 +81,8 @@ function loadTrack(index) {
     playPauseBtn.innerHTML = '<i class="fas fa-pause"></i>';
 
     // Supprimer toutes les classes d'animation de fond
-    document.body.classList.remove('bg-animation-1', 'bg-animation-2');
-
+    document.body.classList.remove('bg-animation-1', 'bg-animation-2', 'bg-animation-3');
+    
     // Ajouter la classe d'animation de fond correspondante à la piste
     document.body.classList.add(tracks[index].backgroundClass);
 }
@@ -89,3 +91,26 @@ function loadTrack(index) {
 
 loadTrack(currentTrackIndex);
 
+document.getElementById('main-title').addEventListener('click', function() {
+  Swal.fire({
+    title: 'Contact',
+    html: `
+      <ul style="list-style: none; padding: 0;">
+        <li><a href="#" onclick="menuItemClick('Option 1')">Youtube</a></li>
+        <li><a href="#" onclick="menuItemClick('Option 2')">Napster</a></li>
+        <li><a href="#" onclick="menuItemClick('Option 3')">Option 3</a></li>
+      </ul>
+    `,
+    showCloseButton: true,
+    showConfirmButton: false,
+    focusConfirm: false,
+  });
+});
+
+function menuItemClick(option) {
+  Swal.fire({
+    title: `You selected ${option}`,
+    icon: 'success',
+    confirmButtonText: 'OK'
+  });
+}
